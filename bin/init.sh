c3d43c2df0b7d195222eb1ff7e5f5a130bc25eaa
panwl <panwl@wondershare.cn>
2019-01-17 14:52:33 +0800

    * add init script

---
+++ bin/init.sh
@@ -0,0 +1,24 @@
+root=$(cd "$(dirname "$0")"; cd ..; pwd)
+cd $root
+
+# 需要创建的目录
+mk_dirs=(
+    application/logics
+    application/models
+    application/services
+    application/views/layout
+    application/views_compile
+    build
+    logs
+)
+
+# 创建目录并设定权限
+for dir in ${mk_dirs[@]}; do
+    if [ ! -d $dir ]; then
+        mkdir -p $dir
+        echo "Created Directory: $dir"
+    fi
+done
+
+# 清空视图缓存文件
+rm -f application/cache/*
